0620b758d1ed5460789cbb5cb9bd31577c286766
jgimprovements-arch <jgimprovements@gmail.com>
2026-04-13 22:29:52 -0500

    Update albi.js

--- api/albi.js
+++ api/albi.js
@@ -1,4 +1,46 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const ALBI_KEY = '135e4c0e-9d1b-46be-8f47-94951e93ffab';
+  const { location, pageSize = '50', page = '1' } = req.query;
+
+  // location param is "Appleton" or "Stevens Point"
+  const endpoints = [
+    'https://api.albiware.com/v5/Integrations/Projects',
+    'https://api.albiware.com/v5/Projects',
+  ];
+
+  for (const base of endpoints) {
+    try {
+      const params = new URLSearchParams({ pageSize, page });
+      if (location) params.set('location', location);
+      const url = `${base}?${params}`;
+
+      const albiRes = await fetch(url, {
+        headers: {
+          'Authorization': `Bearer ${ALBI_KEY}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      });
+
+      if (albiRes.ok) {
+        const data = await albiRes.json();
+        res.setHeader('Cache-Control', 's-maxage=120');
+        return res.status(200).json(data);
+      }
+    } catch (err) {
+      continue;
+    }
+  }
+
+  return res.status(200).json([]);
+}
+export default async function handler(req, res) {
   // Handle preflight CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
